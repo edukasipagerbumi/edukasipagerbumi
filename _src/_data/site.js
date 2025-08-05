ca10d14b48a13d6176bf7c1a49b97b4350074104
edukasipagerbumi <edukasipagerbumi@gmail.com>
2025-08-05 23:58:34 +0700

    input content

---
+++ _src/_data/site.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: "EdukasiPagerbumi",
+  description: "Pusat Edukasi untuk Pelajar dan Warga Desa Pagerbumi. Sebuah persembahan dari Mahasiswa KKN.",
+  url: "https://edukasipagerbumi.netlify.app" // Ganti jika Anda menggunakan domain kustom
+};
